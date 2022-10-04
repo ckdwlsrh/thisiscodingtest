9d165af2b5086bc318376644f22b5b8753d6c32d
ckdwlsrh <ckdwls6504@gmail.com>
2022-10-04 16:59:03 +0900

    implementation

--- problem/implementation/12.py
+++ problem/implementation/12.py
@@ -1,15 +1,31 @@
+
+def check(building):
+    for x, y, a in building:
+        if a == 0: # 기둥
+            if y != 0 and (x - 1, y, 1) not in building and (x, y, 1) not in building and (x,y - 1,0) not in building:
+                return True
+        elif a == 1: # 보
+            if (x + 1, y - 1, 0) not in building and (x, y - 1, 0) not in building and ((x - 1,y, 1) not in building or (x + 1,y,1) not in building):
+                return True
+    return False
+            
+
 def solution(n, build_frame):
-    building = [[0] * (n+1) for _ in range(n+1)]
-    answer = []
-    for x, y, a, b in build_frame:
-        if a == 0 and b == 1 and (building[x][y] >= 1 or y == 0):
-            building[x][y] = 1
-            building[x][y + 1] = 1
-            answer.append([x,y,a])
-        elif a == 1 and b == 1 and (building[x][y] == 1 or building[x + 1][y] == 1 or (building[x + 1][y] == 2 and building[x - 1][y] == 2)):
-            building[x][y] = 2
-            building[x + 1][y] = 2
-            answer.append([x,y,a])
     
-    answer.sort(key=lambda k:(k[0],k[1])
+    answer = set()
+    
+    for x, y, a, b in build_frame:
+        if b:
+            answer.add((x,y,a))
+            if check(answer):
+                answer.remove((x,y,a))
+        else:
+            answer.remove((x,y,a))
+            if check(answer):
+                answer.add((x,y,a))
+
+    answer = list(answer)
+    answer.sort(key=lambda k:(k[0],k[1],k[2]))
     return answer
+
+print(solution(5,[[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]))
